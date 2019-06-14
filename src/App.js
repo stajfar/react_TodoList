60aef13e724676b29a525bea52155081cbbf9a22
stajfr <stajfar@yahoo.com>
2019-06-13 20:33:47 -0700

    first commit4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import uuid from 'uuid';
 
 class  App extends Component {
